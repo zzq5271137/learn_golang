640d1711a4966e7337256c03b44b0ad1411bdaae
zhengqing.zhu <zhengqing.zhu@tcl.com>
2021-04-19 18:41:00 +0800

    1

---
+++ 01-Go语言基础入门和编程思维/01-Go语言基础知识/02-内建变量类型.go
@@ -0,0 +1,18 @@
+package main
+
+/**
+ * Golang的内建变量类型:
+ * 1. 布尔型: bool
+ * 2. 字符型: rune  (不同于Java中的Char是一个字节大小, rune是4个字节大小, 即32位)
+ * 3. 字符串型: string
+ * 4. 整数型:
+ *    (u)int, (u)int8, (u)int16, (u)int32, (u)int64, uintptr
+ *    注: 前面加 (u) 表示无符号整数, 不加表示有符号整数
+ * 5. 字节型: byte  (8位)
+ * 6. 浮点数类型:
+ *    float32, float64, complex64, complex128
+ *    注: complex表示复数
+ */
+
+func main() {
+}
