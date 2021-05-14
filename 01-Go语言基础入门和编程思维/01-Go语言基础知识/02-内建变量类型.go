6ecf53c58b7e9a0354c57c799e90450ffbd7d247
zhengqing.zhu <zhengqing.zhu@tcl.com>
2021-05-14 17:09:31 +0800

    1

--- 01-Go语言基础入门和编程思维/01-Go语言基础知识/02-内建变量类型.go
+++ 01-Go语言基础入门和编程思维/01-Go语言基础知识/02-内建变量类型.go
@@ -13,7 +13,7 @@ import (
  * 3. 字符串型: string
  * 4. 整数型:
  *    (u)int, (u)int8, (u)int16, (u)int32, (u)int64, uintptr
- *    注: 前面加 (u) 表示无符号整数, 不加表示有符号整数
+ *    注: 前面加 u 表示无符号整数, 不加表示有符号整数
  * 5. 字节型: byte  (8位)
  * 6. 浮点数类型:
  *    float32, float64, complex64, complex128
